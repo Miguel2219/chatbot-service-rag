3e49319692e289709961085e1b72b8b0c3b0a47e
Miguel Angel <lmangelryl1512@gmail.com>
2026-03-03 16:59:56 -0500

    fix: Chat-bot prompt was fixed to better responses

--- services/rag_service.py
+++ services/rag_service.py
@@ -17,8 +17,19 @@ class ConversationalRAGEngine():
         retriever = self.vector_storage.as_retriever(search_kwargs={"k": 3})
         docs = retriever.invoke(message) #searcyes ChromaDb with the message as quey, return relevant chunks
         context = "\n\n".join([doc.page_content for doc in docs])
+        system_prompt = f"""
+You are an assistant for a company. Answer questions ONLY based on the provided context from the company's documents.
+
+If the context does not contain enough information to answer the question, 
+respond with: "I don't have information about that in the available documents."
+
+Never use your general knowledge to answer. Only use the context provided.
+
+Context:
+{context}
+"""
         messages = [
-            SystemMessage(content=f"You are a hepful message assistant. Use this context to answer: {context}"),
+            SystemMessage(content=system_prompt),
             *self.chat_history, #spreads the history messages into the list
             HumanMessage(content=message)
         ]
